f2a35096b4b2b5cec0d4558a3c111e5d450004fb
sota <hotphilly1776@yahoo.co.jp>
2024-07-22 21:05:17 +0900

    add if directive to avoid unneeded core install command

--- srcs/requirements/wordpress/tools/entrypoint_connectDB.sh
+++ srcs/requirements/wordpress/tools/entrypoint_connectDB.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
+## When error occurred, stop the script.
 set -e
 
-# Wait for the MariaDB service to be available
+## Wait for the MariaDB service to be available. 
+## ヘルスチェックが終わってても少しディレイがあるのか、直後に接続しようとすると失敗することがある
 until mariadb -h "${WORDPRESS_DB_HOST_NAMEONLY}" -u "${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" -e "SHOW DATABASES;" > /dev/null 2>&1; do
   echo "Waiting for database connection..."
   sleep 3
 done
 
-# WordPress設定を行う
+## WordPressの設定ファイルが存在しない場合、設定ファイルを作成する。
+## ボリュームをマウントしている場合は、設定ファイルが既に存在する可能性があるので、
+## その場合は設定ファイルを新規作成しない。
 if [ ! -f /var/www/html/wp-config.php ]; then
   wp core config \
     --dbname=${WORDPRESS_DB_NAME} \
@@ -16,12 +21,17 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     --dbhost=${WORDPRESS_DB_HOST}
 fi
 
-wp core install \
+## WordPressのインストールがされていない場合、WordPressをインストールする。
+if ! wp core is-installed; then
+  ## WordPressのインストール
+  wp core install \
     --url="${WORDPRESS_URL}" \
     --title="${WORDPRESS_TITLE}" \
     --admin_user="${WORDPRESS_ADMIN_USER}" \
     --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
     --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+    --skip-email
+fi
 
 # php-fpmを起動
 exec sudo -E php-fpm8.2 -F
